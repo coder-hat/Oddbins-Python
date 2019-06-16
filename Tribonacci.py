9552147bef87e7dc95fc559267102130856b765a
coder-hat <coder.hat@gmail.com>
2019-06-15 20:02:31 -0700

    Adds generalized n-nacci function. Also, improved the cmdline options in the __main__.

--- Tribonacci.py
+++ Tribonacci.py
@@ -1,12 +1,20 @@
 '''
-Functions that generate the Tribonacci number sequence:
+This .py is is misnamed: orignally it was for a function
+that generates the Tribonacci number sequence:
 a(n) = a(n-1) + a(n-2) + a(n-3) with a(0)=a(1)=0, a(2)=1.
 OEIS: http://oeis.org/A000073
-Wolfram et al.
+(Wolfram et al.
 http://mathworld.wolfram.com/TribonacciNumber.html
-give different initial conditions: a(0)=0, a(1)=a=(2)=1.
+give different initial conditions: a(0)=0, a(1)=a=(2)=1.)
+
+However, developing the gen_tribonacci() function revealed
+that a generalized function was a trivial modification away.
+Hence: gen_n_nacci_sequence()
 '''
+
+import argparse
 from collections import deque
+
 
 def gen_tribonacci():
     '''
@@ -25,6 +33,35 @@ def gen_tribonacci():
             yield x
             running_terms.append(x)
 
+def gen_n_nacci_sequence(*initial_terms):
+    '''
+    Generalized version of the gen_tribonacci function.
+    This version creates a generator for an n-nacci sequence
+    based on the initial terms provided.
+    (e.g. gen_n_nacci_sequence(0 1) creates a Fibonacci generator.)
+    '''
+    initial_terms = deque(initial_terms)
+    running_terms = deque(initial_terms, len(initial_terms))
+    while True:
+        if initial_terms:
+            x = initial_terms.popleft()
+            yield x
+            running_terms.append(x)
+        else:
+            x = sum(running_terms)
+            yield x
+            running_terms.append(x)
+
+
 if __name__ == "__main__":
-    tri = gen_tribonacci()
-    print([next(tri) for i in range(25)])
+    msg = (
+        'Prints the terms of the n-nacci sequence defined by the initial terms.\n'
+        'Example: the initial terms 0 1 prints the Fibonacci sequence.'
+    )
+    parser = argparse.ArgumentParser(msg)
+    parser.add_argument('-n', type=int, nargs='?', default=10, help='number of terms to print')
+    parser.add_argument('initial_terms', type=int, default=[0, 1], nargs='*')
+    args = parser.parse_args()
+
+    nacci_generator = gen_n_nacci_sequence(*args.initial_terms)
+    print([next(nacci_generator) for i in range(args.n)])
